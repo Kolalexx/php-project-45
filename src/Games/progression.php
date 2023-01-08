c25f35871b5cec99a2409829213e4810802abf23
Aleksei Kolesnikov <kolalexx@yandex.ru>
2023-01-08 07:22:37 +0300

    fixed game brain-progression

--- src/Games/progression.php
+++ src/Games/progression.php
@@ -4,39 +4,31 @@ namespace BrainGames\src\Games\progression;
 
 use function cli\line;
 use function cli\prompt;
-use function BrainGames\src\Engine\greeting;
+use function BrainGames\src\Engine\goPlay;
 use function BrainGames\src\Engine\randomNumbers1;
 use function BrainGames\src\Engine\randomNumbers2;
-use function BrainGames\src\Engine\wrong;
-use function BrainGames\src\Engine\congratulations;
 
-function gameFourth()
+function findMissingNumberInProgression()
 {
-    line('Welcome to the Brain Games!');
-    $name = prompt('May I have your name?');
-    greeting($name);
-    line('What number is missing in the progression?');
-    $index = 0;
-    while ($index < 3) {
-        $numbers1 = randomNumbers1();
-        $numbers2 = randomNumbers2();
-        $numbers3 = randomNumbers2();
-        $arr = [];
-        for ($i = 0; $i < 10; $i++) {
-            $arr[$i] = $numbers1 + $i * $numbers2;
+    $arrayQuestion = [];
+    $arrayRightAnswer = [];
+    for ($i = 0; $i < 3; $i++) {
+        $randomNumber1 = randomNumbers1();
+        $randomNumber2 = randomNumbers2();
+        $randomNumber3 = randomNumbers2();
+        $progression = [];
+        for ($j = 0; $j < 10; $j++) {
+            $progression[$j] = $randomNumber1 + $j * $randomNumber2;
         }
-        $result = $arr[$numbers3];
-        $arr[$numbers3] = '..';
-        $question = implode(' ', $arr);
-        line("Question: %s!", $question);
-        $answer = prompt('Your answer');
-        if ($answer == $result) {
-            line('Correct!');
-            $index += 1;
-        } else {
-            line("'$answer' is wrong answer ;(. Correct answer was '$result'.");
-            return wrong($name);
-        }
+        $missingNumber = $progression[$randomNumber3];
+        $progression[$randomNumber3] = '..';
+        $question = implode(' ', $progression);
+        $arrayQuestion[$i] = $question;
+        $arrayRightAnswer[$i] = $missingNumber;
     }
-    congratulations($name);
+
+    $condition = 'What number is missing in the progression?';
+
+    goPlay($arrayQuestion, $arrayRightAnswer, $condition);
+
 }
