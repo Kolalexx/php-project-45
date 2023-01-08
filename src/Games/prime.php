6835f17b0060d9634fa607748369b84d5d52be6f
Aleksei Kolesnikov <kolalexx@yandex.ru>
2023-01-08 07:54:34 +0300

    fixed game brain-prime

--- src/Games/prime.php
+++ src/Games/prime.php
@@ -4,37 +4,25 @@ namespace BrainGames\src\Games\prime;
 
 use function cli\line;
 use function cli\prompt;
-use function BrainGames\src\Engine\greeting;
+use function BrainGames\src\Engine\goPlay;
 use function BrainGames\src\Engine\randomNumbers1;
-use function BrainGames\src\Engine\wrong;
-use function BrainGames\src\Engine\congratulations;
 
-function gameFifth()
+function checkIfNumberIsPrime()
 {
-    line('Welcome to the Brain Games!');
-    $name = prompt('May I have your name?');
-    greeting($name);
-    line('Answer "yes" if given number is prime. Otherwise answer "no".');
-    $index = 0;
-    $arr = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47];
-    while ($index < 3) {
+    $arrayQuestion = [];
+    $arrayRightAnswer = [];
+    $primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47];
+    for ($i = 0; $i < 3; $i++) {
         $question = randomNumbers1();
-        line("Question: %s!", $question);
-        $answer = prompt('Your answer');
-        if (
-            (in_array($question, $arr, true) && ($answer === "yes")) ||
-            (!in_array($question, $arr, true) && ($answer === "no"))
-        ) {
-            line('Correct!');
-            $index += 1;
+        if (in_array($question, $primeNumbers, true)) {
+            $arrayRightAnswer[$i] = 'yes';
         } else {
-            if (in_array($question, $arr, true)) {
-                line("'$answer' is wrong answer ;(. Correct answer was 'yes'.");
-            } else {
-                line("'$answer' is wrong answer ;(. Correct answer was 'no'.");
-            }
-            return wrong($name);
+            $arrayRightAnswer[$i] = 'no';
         }
+        $arrayQuestion[$i] = $question;
     }
-    congratulations($name);
+
+    $condition = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+    goPlay($arrayQuestion, $arrayRightAnswer, $condition);
 }
