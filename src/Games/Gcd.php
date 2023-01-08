2ba516169398044f7cf273c5dccfb3e5730c2ee9
Aleksei Kolesnikov <kolalexx@yandex.ru>
2023-01-08 06:54:19 +0300

    fixed game brain-gcd

--- src/Games/Gcd.php
+++ src/Games/Gcd.php
@@ -4,56 +4,37 @@ namespace BrainGames\src\Games\Gcd;
 
 use function cli\line;
 use function cli\prompt;
-use function BrainGames\src\Engine\greeting;
+use function BrainGames\src\Engine\goPlay;
 use function BrainGames\src\Engine\randomNumbers1;
 use function BrainGames\src\Engine\randomNumbers2;
-use function BrainGames\src\Engine\wrong;
-use function BrainGames\src\Engine\congratulations;
 
-function gameThird()
+function findGreateasCommonDivisor()
 {
-    line('Welcome to the Brain Games!');
-    $name = prompt('May I have your name?');
-    greeting($name);
-    line('Find the greatest common divisor of given numbers.');
-    $index = 0;
-    while ($index < 3) {
-        $numbers1 = randomNumbers1();
-        $numbers2 = randomNumbers2();
-        $result = 0;
-        $question = "$numbers1 $numbers2";
-        if ($numbers1 === $numbers2) {
-            $result = $numbers1;
-        } elseif ($numbers1 > $numbers2) {
-            $i = 1;
-            while ($i <= $numbers2) {
-                if ($numbers1 % $i === 0 && $numbers2 % $i === 0) {
-                    $result = $i;
-                    $i += 1;
-                } else {
-                    $i += 1;
-                }
-            }
+    $arrayQuestion = [];
+    $arrayRightAnswer = [];
+    for ($i = 0; $i < 3; $i++) {
+        $randomNumber1 = randomNumbers1();
+        $randomNumber2 = randomNumbers2();
+        $result = 1;
+        $question = "$randomNumber1 $randomNumber2";
+        if ($randomNumber1 === $randomNumber2) {
+            $result = $randomNumber1;
         } else {
-            $i = 1;
-            while ($i <= $numbers1) {
-                if ($numbers2 % $i === 0 && $numbers1 % $i === 0) {
-                    $result = $i;
-                    $i += 1;
+            $greteasCommonDivisor = 1;
+            while ($greteasCommonDivisor <= $randomNumber2) {
+                if ($randomNumber1 % $greteasCommonDivisor === 0 && $randomNumber2 % $greteasCommonDivisor === 0) {
+                    $result = $greteasCommonDivisor;
+                    $greteasCommonDivisor += 1;
                 } else {
-                    $i += 1;
+                    $greteasCommonDivisor += 1;
                 }
             }
         }
-        line("Question: %s!", $question);
-        $answer = prompt('Your answer');
-        if ($answer == $result) {
-            line('Correct!');
-            $index += 1;
-        } else {
-            line("'$answer' is wrong answer ;(. Correct answer was '$result'.");
-            return wrong($name);
-        }
+        $arrayQuestion[$i] = $question;
+        $arrayRightAnswer[$i] = $result;
     }
-    congratulations($name);
+
+    $condition = 'Find the greatest common divisor of given numbers.';
+
+    goPlay($arrayQuestion, $arrayRightAnswer, $condition);
 }
