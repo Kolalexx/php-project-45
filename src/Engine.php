708565bc8da6399529eb2a02d4f97d737ee897ac
Aleksei Kolesnikov <kolalexx@yandex.ru>
2023-01-12 06:11:56 +0300

    fixed Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -5,11 +5,6 @@ namespace BrainGames\src\Engine;
 use function cli\line;
 use function cli\prompt;
 
-function greeting(string $name)
-{
-    line("Hello, %s!", $name);
-}
-
 function randomNumbers1()
 {
     $minNumber = 2;
@@ -22,16 +17,6 @@ function randomNumbers2()
     $minNumber = 1;
     $maxNumber2 = 9;
     return (rand($minNumber, $maxNumber2));
-}
-
-function wrong(string $name)
-{
-    line("Let's try again, %s!", $name);
-}
-
-function congratulations(string $name)
-{
-    line("Congratulations, %s!", $name);
 }
 
 function randomOperation()
